0d665b40ad84c5ae120653eb68b67f76bec18fc9
smitha kamath <smitha.bhandarkar2014@gmail.com>
2023-01-17 23:47:25 +0200

    parses through only partial array

--- Program.cs
+++ Program.cs
@@ -13,15 +13,16 @@ namespace CSharpFundamental
             Console.WriteLine(newText); // expect to see "This Is A Text"
 
             /* Challenge 2 */
-            // int[,] arrayA = { { 3, 5, 4 }, { 3, 7, 8 } };
-            // int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
-            // int[,] result = matrixMultiply(arrayA, arrayB);
-           //Console.WriteLine(result); //share your findings to Slack
+            int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
+            int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
+            int[,] result = matrixMultiply(arrayA, arrayB);
+            Console.WriteLine(result); //share your findings to Slack
         }
         static string toTitleCase(string input)
         {
             string[] justStringArr = input.Split(' ');
-            char[] upperCase = new char[30]; 
+            //List<string> listStr = new List<string>();
+            char[] upperCase = new char[30];
             int upperCaseCounter = default(int);
             int counter = default(int);
             var builder = new System.Text.StringBuilder();
@@ -44,21 +45,32 @@ namespace CSharpFundamental
                 {
                     if (strFirstLetter == i)
                     {
-                        char newChar = upperCase[counter];
+                        char newUpperChar = upperCase[counter];
                         string restString = str.Substring(1, str.Length - 1);
-                        builder.Append(str.Substring(0, 1).Replace(strFirstLetter, newChar) + restString);
+                        string newStr = string.Concat(str.Substring(0, 1).Replace(strFirstLetter, newUpperChar), restString);
+                        builder.Append(newStr);
                         break;
                     }
                     counter = counter + 1;
                 }
                 builder.Append(' ');
             }
-            //Console.WriteLine("{1}", builder.Length, builder.ToString());
             return builder.ToString();
         }
-        // static int[,] matrixMultiply(int[,] array1, int[,] array2)
-        // {
-        //     /* Write your code here */
-        // }
+        static int[,] matrixMultiply(int[,] array1, int[,] array2)
+        {
+            int[,] newArr = new int[2, 2];
+            for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                int data = default(int);
+                for (int j = 0; j < array2.GetLength(0); j++)
+                {
+                    data += array1[i, j] * array2[j, i];
+                }
+                Console.Write(data + " ");
+                Console.WriteLine();
+            }
+            return newArr;
+        }
     }
 }
